68907f2bc7388d182d836afbafd0abbc9e170cb2
Mfeliciano778 <1652@holbertonschool.com>
2020-02-05 20:56:55 +0000

    A C program that prints a string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+/**
+ * main - function that prints a string
+ *
+ *
+ * Return: 0
+ */
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
