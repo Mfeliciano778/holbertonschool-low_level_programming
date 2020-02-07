861490840b9694d6b20813dfbc0b0407e9a01884
Mfeliciano778 <1652@holbertonschool.com>
2020-02-07 18:05:08 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - function that prints the alphabet in lowercase,
+* followed by a new line
+*
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char let1;
+
+	let1="a";
+
+	while (let1 <= "z", let1 != "q" && let1 != "e")
+	{
+		putchar(let1);
+		let1++;
+	}
+	putchar("\n"); 
+	return (0);
+}
