88d86150fbecce8b56e420c5a9505f5af6f6d9c9
Mfeliciano778 <1652@holbertonschool.com>
2020-02-10 17:48:45 +0000

    A program that prints Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * main - function prints Holberton followed
+ * by a new line
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+	
+	return (0);
+}
