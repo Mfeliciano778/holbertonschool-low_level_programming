f2f2cf076462bae8ff99a6c187e9207d98c53c47
Mfeliciano778 <1652@holbertonschool.com>
2020-02-11 01:33:40 +0000

    A program that prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - function that computes the absolute value of an integer
+ *
+ *
+ *
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int num;
+	int num2;
+	int num3;
+	int num4
+
+	for (num = '0'; num <= '1'; num++)
+	{
+
+	for (num2 = '0'; num2 <= '2'; num2++)
+	{
+
+	for (num3 = '0'; num3 <= '5'; num3++)
+	{
+
+	for (num4 = '0'; num4 <= '9'; num4++)
+	{
+		putchar(num1);
+		putchar(num2);
+		putchar(':');
+		putchar(num3);
+		putchar(num4);
+	}
+	}
+	}
+	}
+}
