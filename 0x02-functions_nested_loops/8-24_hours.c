762253c098db4b6608c4228ffbef6273f7c8f2b4
Mfeliciano778 <1652@holbertonschool.com>
2020-02-11 19:14:44 +0000

    A function that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * main - function that computes the absolute value of an integer
- *
- *
+ * jack_bauer - function that computes the absolute value of an integer
  *
  *
  * Return: 0
@@ -13,28 +11,34 @@
 void jack_bauer(void)
 {
 	int num;
+
 	int num2;
+
 	int num3;
-	int num4
 
-	for (num = '0'; num <= '1'; num++)
+	int num4;
+
+	for (num = '0'; num <= '2'; num++)
 	{
 
-	for (num2 = '0'; num2 <= '2'; num2++)
-	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			if (num >= '2' && num2 >= '4')
+				break;
 
-	for (num3 = '0'; num3 <= '5'; num3++)
-	{
+			for (num3 = '0'; num3 <= '5'; num3++)
+			{
 
-	for (num4 = '0'; num4 <= '9'; num4++)
-	{
-		putchar(num1);
-		putchar(num2);
-		putchar(':');
-		putchar(num3);
-		putchar(num4);
-	}
-	}
-	}
-	}
+				for (num4 = '0'; num4 <= '9'; num4++)
+				{
+					putchar(num);
+					putchar(num2);
+					putchar(':');
+					putchar(num3);
+					putchar(num4);
+					putchar('\n');
+				}
+				}
+				}
+				}
 }
