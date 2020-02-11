4f1f240c9e29a0c6282328f738afef5c5dfc050e
Mfeliciano778 <1652@holbertonschool.com>
2020-02-11 20:19:01 +0000

    A function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+nclude <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ *
+ * @num: variable used to pass numbers into it
+ *
+ *
+ * Return: 0
+ */
+
+int _add(int a + int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
