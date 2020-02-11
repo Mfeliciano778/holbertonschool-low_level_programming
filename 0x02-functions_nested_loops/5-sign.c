bda3155082e5931cd74235678071b4c64502af28
Mfeliciano778 <1652@holbertonschool.com>
2020-02-11 15:13:47 +0000

    A function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
-		putchar('+');
 	}
 	if (n == 0)
 	{
+		_putchar('0');
 		return (0);
-		putchar('0');
 	}
 	if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
-		putchar('-');
 	}
 	return (0);
 }
