d07c7206d9c5367b09d3bdeef7b7f5356e9054b2
Mfeliciano778 <1652@holbertonschool.com>
2020-02-13 18:49:37 +0000

    Hoberton prototype files

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -7,5 +7,11 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int _isupper(char c);
 int _isdigit(int n);
+int mul(int num1, int num2);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
 
 #endif /* HOLBERTON_H */
