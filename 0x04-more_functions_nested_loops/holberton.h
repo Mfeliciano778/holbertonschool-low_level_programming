ebefd39efa4e0ccfefc968247fc3ebea5e3472c2
Mfeliciano778 <1652@holbertonschool.com>
2020-02-13 15:06:29 +0000

    Holberton prototype File

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int _isupper(char c);
+int _isdigit(int n);
+
+#endif /* HOLBERTON_H */
