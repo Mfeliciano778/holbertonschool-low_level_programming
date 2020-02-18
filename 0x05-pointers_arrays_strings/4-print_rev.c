ac4554c691746665b79b3fd3002dd701923b3947
Mfeliciano778 <1652@holbertonschool.com>
2020-02-18 18:00:41 +0000

    A function that prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: pointer we are getting the string from`
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+	int rev;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	count--;
+	rev = count;
+	while (rev >= 0)
+	{
+		rev--;
+		_putchar(s[rev]);
+	}
+	_putchar('\n');
+}
