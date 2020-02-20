7b826a7514bb35d6e68ed7e90a93cbd345401879
Mfeliciano778 <1652@holbertonschool.com>
2020-02-20 18:52:32 +0000

    A function that concatenates two strings using n as the outcome

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strncat - concatenates two unknown integers
+ *
+ *
+ *
+ *
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int d_len;
+	int index;
+
+	for (d_len = 0; dest[d_len] != '\0'; d_len++)
+		;
+	for (index = 0; index < n && src[index] != '\0'; index++)
+		dest[d_len + index] = src[index];
+
+	dest[d_len + index] = '\0';
+
+	return (dest);
+
+	putchar(10);
+}
