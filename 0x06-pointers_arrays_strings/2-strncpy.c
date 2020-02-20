439c768d44eec403b290fa1bd32dbfaef2d3c73e
Mfeliciano778 <1652@holbertonschool.com>
2020-02-20 19:07:56 +0000

    A function that concatenates a number of bytes between two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,8 +4,9 @@
 /**
  * *_strncpy - function copies a string.
  *
- *
- *
+ * @dest: pointer we are concatenating to.
+ * @src: pointer we are concatenating from.
+ * @n: number of bytes we are concatenating.
  *
  * Return: Always 0.
  */
@@ -16,7 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (len = 0; len < n && src[len] != 0; len++)
 		dest[len] = src[len];
 
-	for(; len < n; len++)
+	for (; len < n; len++)
 		dest[len] = '\0';
 
 	return (dest);
