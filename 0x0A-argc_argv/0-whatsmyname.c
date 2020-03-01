2305951843cabc0e502e8c327c4052908dc68c2e
Mfeliciano778 <1652@holbertonschool.com>
2020-03-01 17:35:56 +0000

    A program that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - function that prints the name of the program in the output.
+ * @argc: a count of the arguments supplied to the program.
+ * @argv: an array of pointers to the strings which are those arguments.
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
