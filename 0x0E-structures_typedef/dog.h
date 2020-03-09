db7217d2db95b1dea6120dc461520357b73e682c
Mfeliciano778 <1652@holbertonschool.com>
2020-03-09 18:49:25 +0000

    Defines a new type struct dog with name, age, and owner as elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new struct called dog
+ * @name: name of dog
+ * @owner: owner of dog
+ * @age: age of dog
+ *
+ * Return: Always 0
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
