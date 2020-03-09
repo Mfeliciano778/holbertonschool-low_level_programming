e1bd853ded01cf8fb0a016d06251ca6648197cb1
Mfeliciano778 <1652@holbertonschool.com>
2020-03-09 20:03:54 +0000

    Added dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,26 +15,9 @@ struct dog
 	char *owner;
 	float age;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#endif
-#ifndef DOG_T
-#define DOG_T
-
-/**
- * struct dog - new struct called dog
- * @name: name of dog
- * @owner: owner of dog
- * @age: age of dog
- *
- * Return: Always 0
- */
-typedef struct dog dog_t
-{
-        char *name;
-        char *owner;
-        float age;
-} dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
