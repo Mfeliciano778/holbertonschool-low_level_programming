8e1767df8ffe98729988d7c07b834554e23d88bf
Mfeliciano778 <1652@holbertonschool.com>
2020-03-10 17:12:11 +0000

    A function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+char *_strdup(char *str);
+/**
+ * *new_dog - function that creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	char *nmecpy;
+	char *owncpy;
+
+	dog = malloc(sizeof(dog_t));
+		if(!dog)
+			return (NULL);
+	nmecpy = _strdup(name);
+	owncpy = _strdup(owner);
+
+	if (!nmecpy || !owncpy)
+	{
+		free(owncpy);
+		free(nmecpy);
+		free(dog);
+		free(NULL);
+	}
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
+/**
+ * *_strdup - function returns a pointer to a newly allocated space in 
+ * memory
+ * @str: pointer with copy of the string given as a parameter
+ *
+ *
+ * Return: Always 0
+ */
+char *_strdup(char *str)
+{
+
+	int i;
+	int counter = 0;
+	char *clonestr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (counter = 0; str[counter]; counter++)
+		;
+
+	clonestr = malloc(counter * sizeof(*str));
+		if (clonestr == NULL)
+			return (NULL);
+
+	for (i = 0; i < counter; i++)
+		clonestr[i] = str[i];
+
+	clonestr[i] = '\0';
+
+	return (clonestr);
+}
