d2dc905b02e857ae1435b45e19637b30902fa9e0
Mfeliciano778 <1652@holbertonschool.com>
2020-03-10 18:21:04 +0000

    Function creates new String

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,9 +29,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	dog->name = name;
+	dog->name = nmecpy;
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = owncpy;
 
 	return (dog);
 }
