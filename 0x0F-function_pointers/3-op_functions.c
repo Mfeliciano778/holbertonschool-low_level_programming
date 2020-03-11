6e22fb8edb6a774a5d95c159d1830adad49c8d6f
Mfeliciano778 <1652@holbertonschool.com>
2020-03-11 21:11:52 +0000

    Functions contain all the 5 functions our calculator usesgp

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - function adds to integers
+ * @a: first integer were adding
+ * @b: second integer were adding
+ *
+ * Return: Always 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function defines the modulo of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
