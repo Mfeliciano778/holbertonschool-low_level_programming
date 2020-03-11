8110a2055aa9e3409ab3e5c24e5e1f81f8e1a4a3
Mfeliciano778 <1652@holbertonschool.com>
2020-03-11 18:15:51 +0000

    a function that executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - function that executes a function given as a parameter
+ * on each element of an array
+ * @array: pointer array
+ * @action: pointer of the elements
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int index;
+
+	if (action != NULL)
+		for(index = 0; index < size; index++)
+			action(array[index]);
+}
