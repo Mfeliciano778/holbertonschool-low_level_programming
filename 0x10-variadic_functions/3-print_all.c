e0abebb049f0a37e45aa80717981f0fc2851607c
Mfeliciano778 <1652@holbertonschool.com>
2020-03-18 19:04:07 +0000

    Intial commmit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_all - function that prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+
+}
