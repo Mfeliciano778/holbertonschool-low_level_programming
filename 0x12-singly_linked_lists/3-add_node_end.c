9c5f13aee78b1a66734711a37795ab9d68eb3b63
Mfeliciano778 <1652@holbertonschool.com>
2020-03-22 17:02:42 +0000

    Initial commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,10 +4,39 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * *add_node_end - function that adds a new node at the end of a list_t list
  *
  * Return: Always 0.
  */
-int main(void)
+list_t *add_node_end(list_t **head, const char *str)
 {
+	int leng = 0;
+	list_t *tmp, *end;
+	char *ptr = strdup(str);
+
+	if (!str)
+		return (NULL);
+	tmp = malloc(sizeof(list_t));
+		if (tmp == NULL)
+			return (NULL);
+	while (str[leng])
+	{
+		++leng;
+	}
+	tmp->str = ptr;
+	tmp->len = leng;
+	tmp->next = NULL;
+	end = *head;
+
+	if (!*head)
+	{
+		*head = tmp;
+		return (*head);
+	}
+	while (end->next != NULL)
+	{
+		end = end->next;
+	}
+	end->next = tmp;
+	return (tmp);
 }
