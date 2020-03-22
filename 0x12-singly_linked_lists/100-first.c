d27b6ab6cc9ce8de273c959c1ea0c3128d82c994
Mfeliciano778 <1652@holbertonschool.com>
2020-03-22 17:27:48 +0000

    Initial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void turtle_hare(void) __attribute__ ((constructor));
+
+/**
+ * turtle_hare - prints message before main
+ */
+void turtle_hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
