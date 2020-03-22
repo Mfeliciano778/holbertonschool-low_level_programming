13cda7412bd3b39060cea74d6292c1a69a7cd29d
Mfeliciano778 <1652@holbertonschool.com>
2020-03-22 16:50:46 +0000

    Initial commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,10 +4,26 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * *add_node - function addes a new node at the beginning of a list_t list
  *
  * Return: Always 0.
  */
-int main(void)
+list_t *add_node(list_t **head, const char *str)
 {
+	int leng = 0;
+	list_t *tmp;
+	char *ptr = strdup(str);
+
+	tmp = malloc(sizeof(list_t));
+		if (tmp == NULL || str == NULL)
+			return (NULL);
+	while (str[leng])
+	{
+		++leng;
+	}
+	tmp->len = leng;
+	tmp->str = ptr;
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
 }
