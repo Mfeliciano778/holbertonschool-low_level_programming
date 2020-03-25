5a1305729e5bc998875af637143d882d5b03b59c
Mfeliciano778 <1652@holbertonschool.com>
2020-03-25 13:12:57 +0000

    Cleaning up my code so that it's more effiecent

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a list_t list
+ * @h: pointer where the list is located
+ *
+ * Return: Always 0.
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (!h)
+		return (0);
+
+	printf("%i\n", h->n);
+	
+	return (print_listint(h->next) + 1);
+}
