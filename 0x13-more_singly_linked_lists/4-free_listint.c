e0ead6308e130e0dd4ce55db5b10b8ba78292f9b
Mfeliciano778 <1652@holbertonschool.com>
2020-03-25 18:36:57 +0000

    Added the code to fix more bugs

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a linked list
+ * @head: pointer that starts the linked list
+ *
+ *
+ * Return: Always 0
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	else
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
