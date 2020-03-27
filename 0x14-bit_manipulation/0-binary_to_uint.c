dd57c4782500022715fa18ec16528a5c5a2a9f9a
Mfeliciano778 <1652@holbertonschool.com>
2020-03-27 03:57:58 +0000

    Inital commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - function that converts a binary numbert to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: Always 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+}
