c38729a7dcb4960966c14cae95076c1b601b993f
Mfeliciano778 <1652@holbertonschool.com>
2020-03-30 00:39:21 +0000

    Fixed corrupt file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,5 +7,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int char_holder = 0;
+	int count;
 
+	if (!b)
+		return (0);
+
+	for (count = 0; b[count]; count++)
+	{
+		if (b[count] == '1')
+			char_holder = char_holder * 2 + 1;
+		else if (b[count] == '0')
+			char_holder *= 2;
+		else
+			return (0);
+	}
+	return (char_holder);
 }
