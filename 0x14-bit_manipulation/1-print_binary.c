2a1a8951741eefd399cd5f42b4f2eff9ca5d902c
Mfeliciano778 <1652@holbertonschool.com>
2020-03-30 00:48:37 +0000

    Made betty compliant

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,23 @@
-llll
+#include "holberton.h"
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: the number that gets passed
+ *
+ * Return: always 0
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int n_cpy = n >> 1;
+	unsigned long int num = 1;
+
+	while (n_cpy != 0)
+	{
+		num = num << 1;
+		n_cpy = n_cpy >> 1;
+	}
+	while (num != 0)
+	{
+		_putchar('0' + ((n & num) == num));
+		num = num >> 1;
+	}
+}
