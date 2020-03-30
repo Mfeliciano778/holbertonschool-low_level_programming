d1f70b0bb8c3e6ed4ea0ada45fdba4fda1b5d01c
Mfeliciano778 <1652@holbertonschool.com>
2020-03-30 14:24:57 +0000

    Made betty compliant

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - function prints all single digit numbers
- * of base 10 starting from 0
- *
+ * main - function prints all single digit numbers of base 10 starting from 0
  *
  * Return: 0
  */
