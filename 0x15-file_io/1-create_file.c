acc5c6669c18d7121d0146c68e1308687eb0fc8f
Mfeliciano778 <1652@holbertonschool.com>
2020-04-01 15:14:40 +0000

    Initial commit of function that creates another file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_descriptor = open (filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
 		if (file_descriptor < 0)
 			return (-1);
 	if (text_content == NULL)
