dc4f2bfd93415fa1d7a3f14f7ebfaae8d0374bb2
Mfeliciano778 <1652@holbertonschool.com>
2020-05-06 18:00:09 +0000

    initial commit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * *get_dnodeint_at_index - function returns the nth node of doubly linked list
+ * @head: pointer to the head of doubly linked list
+ * @index: location of node
+ *
+ * Return: Always 0
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head == NULL)
+		return (NULL);
+
+	for(; index != 0; index--)
+	{
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
