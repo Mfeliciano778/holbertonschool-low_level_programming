11983d50199fc01f483fe3529eff0c6f9c25bd97
Mfeliciano778 <1652@holbertonschool.com>
2020-05-06 15:12:08 +0000

    Made betty compliant

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,12 +12,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *last = *head;
 
 	if (head == NULL)
-		return(NULL);
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 		if (new_node == NULL)
 			return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = NULL;
 
@@ -25,12 +25,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
-		return(new_node);
+		return (new_node);
 	}
 
 	while (last->next != NULL)
 		last = last->next;
-	
+
 	new_node->prev = last;
 	last->next = new_node;
 
