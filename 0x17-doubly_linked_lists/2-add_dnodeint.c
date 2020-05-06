483f216a68c3dc87a2f15a9de83106e79229ea0d
Mfeliciano778 <1652@holbertonschool.com>
2020-05-06 15:00:00 +0000

    Fixed seg fault

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,8 +10,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	if (*head == NULL)
-		return (0);
+	if (head == NULL)
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 		if (new_node == NULL)
@@ -20,6 +20,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = *head;
 	new_node->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new_node;
 	*head = new_node;
 
 	return (new_node);
