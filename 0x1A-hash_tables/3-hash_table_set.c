cb800fa69840452c18db66f69928f22a142b4f48
Mfeliciano778 <1652@holbertonschool.com>
2020-05-29 23:33:51 +0000

    Deleted extra unnecessary lines

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,11 +1,9 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_set - function adds an element to the hash table
  * @ht: hash table we are adding the element to
  * @key: the key
  * @value: value associated with the key
- *
  * Return: 1 is succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -15,21 +13,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || ht == NULL || value == NULL)
 		return (0);
-
 	slot = key_index((const unsigned char *)key, ht->size);
-
 	entry = ht->array[slot];
-
 	while (entry != NULL)
 	{
 		if (strcmp(entry->key, key) == 0)
 		{
 			free(entry->value);
-
 			entry->value = strdup(value);
 				if (entry->value == NULL)
 					return (0);
-
 			return (1);
 		}
 		entry = entry->next;
