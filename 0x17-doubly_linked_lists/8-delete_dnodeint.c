a4e0bcfdbb96f16b007f740c3a66af4ad86ae750
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-12 22:55:21 +0000

    Got rid of useless test case

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *curr = (*head);
 	unsigned int i;
 
-	if (!(*head) && index <= 0)
+	if (!(*head))
 	{
 		return (0);
 	}
