5fdadbb1f5b872297b9c04c08f29be68978b0edd
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-12 22:23:01 +0000

    Changed the way I traverse through list from for to while loop, added a variable to index through

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *curr = head;
-	int i;
+	unsigned int i;
 
 	for (i=0; curr != NULL && i < index; i++)
 		curr = curr->next;
