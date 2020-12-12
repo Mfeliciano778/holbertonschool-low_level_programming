d2bed78fc39ccf51bfea54c4926b42e650762968
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-12 22:29:47 +0000

    Made i start at 1 to fix the position of the new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	curr = (*h);
 
-	for (i = 0; curr != NULL && i < idx; i++)
+	for (i = 1; curr != NULL && i < idx; i++)
 		curr = curr->next;
 
 	if (curr->next != NULL)
