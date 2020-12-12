2ba041804eac7a5b2d3d99a8afc285073e4676dc
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-12 21:15:45 +0000

    Gives you the address to the index of a doubly linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Function returns the nth node of doubly linked list
+ * @head: pointer to the doubly linked list
+ * @index: where the node we reutrn is
+ *
+ * Return: data inside the node, or NULL if it failed
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *curr = head;
+
+	while(curr->next != NULL && index)
+	{
+		curr = curr->next;
+		index--;
+	}
+	return(curr);
+}
