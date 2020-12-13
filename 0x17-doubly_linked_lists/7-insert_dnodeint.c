4f28c2399a711960824f5940fde0d1c189003921
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-13 22:48:53 +0000

    Deleted both test cases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,9 +15,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	curr = *h;
 
-	if ((*h) == NULL)
-		return (NULL);
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
