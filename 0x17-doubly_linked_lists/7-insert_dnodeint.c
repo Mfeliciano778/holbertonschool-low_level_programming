7d25a79144644769c48c580f9e2bef1004496d9b
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-13 20:04:58 +0000

    Changed everything

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 
 	new->next = curr;
-	new->prev = curr->next;
+	new->prev = curr->prev;
 	curr->prev->next = new;
 	curr->prev = new;
 
