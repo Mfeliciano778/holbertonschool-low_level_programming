5e52385efd98dc2159ddb59f7cc2319c372e549f
Mfeliciano778 <mattfeliciano778@gmail.com>
2020-12-13 22:47:45 +0000

    Adding more test cases for number 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	curr = *h;
 
+	if ((*h) == NULL || h == NULL)
+		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
