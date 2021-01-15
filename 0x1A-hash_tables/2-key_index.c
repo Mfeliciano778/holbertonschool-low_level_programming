3b3cfc2cc86e75e3bf5056b631f0683a4bb6e5bd
Mfeliciano778 <mattfeliciano778@gmail.com>
2021-01-15 17:21:21 +0000

    Got rid of uneccessary parentheses

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,8 +9,9 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned int value = hash_djb2(key);
+	unsigned long int value;
 
-	value = value % size;
+	value = hash_djb2(key) % size;
+
 	return (value);
 }
